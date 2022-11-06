da634bb6cce30c766deb3ab336cd9dac617b8f3b
MyIDDQD <nickkosh@gmail.com>
2022-11-06 15:38:05 +0100

    Done Stage 1/5: Arrangement

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,5 +1,18 @@
 package cinema
 
 fun main() {
-    // write your code here
+    val numbers = "1 2 3 4 5 6 7 8 "
+    val chars = "S S S S S S S S "
+
+    println("Cinema:")
+
+    for (a in 0..7 ) {
+        println(
+            if (a == 0) {
+                "  $numbers"
+            } else {
+                "$a $chars"
+            }
+        )
+    }
 }
