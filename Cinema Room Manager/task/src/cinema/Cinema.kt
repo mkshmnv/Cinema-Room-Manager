5f05161a8fcb1b83717b7086ee6822831a7f2c6f
MyIDDQD <nickkosh@gmail.com>
2022-11-08 18:29:23 +0100

    Done Stage 4/5: Menu, please!

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,14 +1,15 @@
 package cinema
 
+var MENU_CHOICE = 0
 var ROWS = 0
 var SEATS = 0
 var ROW_NUMBER = 0
 var SEAT_NUMBER = 0
+var ALL_TICKETS = mutableMapOf<Int, Int>()
 
 fun main() {
     sizeCinema()
-    choseTicket()
-//    payback()
+    menu()
 }
 
 fun sizeCinema() {
@@ -16,8 +17,6 @@ fun sizeCinema() {
     ROWS = readln().toInt()
     println("Enter the number of seats in each row:")
     SEATS = readln().toInt()
-    println(" ")
-    printCinema()
 }
 
 fun choseTicket() {
@@ -25,35 +24,13 @@ fun choseTicket() {
     ROW_NUMBER = readln().toInt()
     println("Enter a seat number in that row:")
     SEAT_NUMBER = readln().toInt()
-    printTicket()
+    ALL_TICKETS[ROW_NUMBER] = SEAT_NUMBER
 }
-
-fun payback() {
-    val totalSeats = ROWS * SEATS
-
-    val profit = when {
-        // Calculate - price of each ticket is 10 dollars
-        (totalSeats < 60) -> totalSeats * 10
-
-        // Calculate - price front half of the rows and 8 dollars for the back half
-        (totalSeats > 60) -> {
-            val firstHalf = ROWS / 2 * SEATS
-            val secondHalf = if (ROWS % 2 == 0) ROWS / 2 * SEATS else (ROWS / 2 + 1) * SEATS
-            firstHalf * 10 + secondHalf * 8
-        }
-
-        // Check any errors
-        else -> throw Exception("Error")
-    }
-
-    println("\nTotal income: $$profit\n")
-}
-
 
 fun printCinema() {
     val seats = mutableListOf<String>()
 
-    println("Cinema:\n")
+    println("\nCinema:")
 
     for (a in 0..ROWS) {
         if (a == 0) {
@@ -67,35 +44,12 @@ fun printCinema() {
             for (i in 1..SEATS) {
                 seats.add("S")
             }
+            if (ALL_TICKETS.containsKey(a)) seats[ALL_TICKETS[a]!!] = "B"
             println(seats.joinToString(" "))
             seats.clear()
         }
     }
-}
-
-fun printTicket() {
-    val seats = mutableListOf<String>()
-
-    println("Cinema:\n")
-
-    for (a in 0..ROWS) {
-        if (a == 0) {
-            for (i in 0..SEATS) {
-                if (i == 0) seats.add(" ") else seats.add(i.toString())
-            }
-            println(seats.joinToString(" "))
-            seats.clear()
-        } else {
-            seats.add("$a")
-            for (i in 1..SEATS) {
-                seats.add("S")
-            }
-            if (a == ROW_NUMBER) seats[SEAT_NUMBER] = "B"
-            println(seats.joinToString(" "))
-            seats.clear()
-        }
-    }
-    println("\nTicket price: $${priceTicket()}")
+    menu()
 }
 
 fun priceTicket() : Int {
@@ -104,12 +58,34 @@ fun priceTicket() : Int {
 
     return when {
         // Calculate - price of each ticket is 10 dollars
-        (totalSeats < 60) -> 10
+        (totalSeats in 1..60) -> 10
 
         // Calculate - price 10 to front half of the rows and 8 dollars for the back half
         (totalSeats > 60) -> if (ROW_NUMBER in firstHalf) 10 else 8
 
         // Check any errors
         else -> throw Exception("Error")
+    }
+}
+
+fun menu() {
+    println("\n1. Show the seats\n2. Buy a ticket\n0. Exit")
+    MENU_CHOICE = readln().toInt()
+    when (MENU_CHOICE) {
+        1 -> printCinema()
+        // Buy a ticket and save value
+        2 -> {
+            choseTicket()
+            println("Ticket price: $${priceTicket()}")
+            menu()
+        }
+        // Exit from program and clear all
+        0 -> {
+            MENU_CHOICE = 0
+            ROWS = 0
+            SEATS = 0
+            ROW_NUMBER = 0
+            SEAT_NUMBER = 0
+        }
     }
 }
