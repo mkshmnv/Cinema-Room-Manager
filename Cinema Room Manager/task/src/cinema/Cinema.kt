1ba6582069276d367ac92b0164f9709887709d59
MyIDDQD <nickkosh@gmail.com>
2022-11-10 08:05:40 +0100

    Done with math error Stage 5/5: Errors!

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -5,19 +5,46 @@ var ROWS = 0
 var SEATS = 0
 var ROW_NUMBER = 0
 var SEAT_NUMBER = 0
-var ALL_TICKETS = mutableMapOf<Int, Int>()
+var ALL_TICKETS = mutableListOf(listOf<Int>())
 var SUM_PURCHASED_TICKETS = 0
 
 fun main() {
-    sizeCinema()
+    startCinema()
     menu()
 }
 
-fun sizeCinema() {
+fun startCinema() {
     println("Enter the number of rows:")
     ROWS = readln().toInt()
     println("Enter the number of seats in each row:")
     SEATS = readln().toInt()
+}
+
+fun menu() {
+    println("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit")
+    MENU_CHOICE = readln().toInt()
+    when (MENU_CHOICE) {
+        1 -> printCinema()
+        // Buy a ticket and save value
+        2 -> {
+            choseTicket()
+            menu()
+        }
+        3 -> {
+            statistics()
+            menu()
+        }
+        // Exit from program and clear all
+        0 -> {
+            MENU_CHOICE = 0
+            ROWS = 0
+            SEATS = 0
+            ROW_NUMBER = 0
+            SEAT_NUMBER = 0
+            ALL_TICKETS.clear()
+            SUM_PURCHASED_TICKETS = 0
+        }
+    }
 }
 
 fun choseTicket() {
@@ -25,7 +52,21 @@ fun choseTicket() {
     ROW_NUMBER = readln().toInt()
     println("Enter a seat number in that row:")
     SEAT_NUMBER = readln().toInt()
-    ALL_TICKETS[ROW_NUMBER] = SEAT_NUMBER
+
+    when {
+        ROW_NUMBER > ROWS || SEAT_NUMBER > SEATS -> {
+            println("\nWrong input!")
+            choseTicket()
+        }
+        ALL_TICKETS.contains(listOf(ROW_NUMBER, SEAT_NUMBER)) -> {
+            println("\nThat ticket has already been purchased!")
+            choseTicket()
+        }
+        else -> {
+            ALL_TICKETS.add(listOf(ROW_NUMBER, SEAT_NUMBER))
+            priceTicket()
+        }
+    }
 }
 
 fun printCinema() {
@@ -42,10 +83,13 @@ fun printCinema() {
             seats.clear()
         } else {
             seats.add("$a")
-            for (i in 1..SEATS) {
-                seats.add("S")
+            for (b in 1..SEATS) {
+                if (ALL_TICKETS.contains(listOf(a, b))) {
+                    seats.add("B")
+                } else {
+                    seats.add("S")
+                }
             }
-            if (ALL_TICKETS.containsKey(a)) seats[ALL_TICKETS[a]!!] = "B"
             println(seats.joinToString(" "))
             seats.clear()
         }
@@ -56,61 +100,34 @@ fun printCinema() {
 fun priceTicket() : Int {
     val totalSeats = ROWS * SEATS
     val firstHalf = 1..ROWS / 2
-    val price = when {
-        // Calculate - price of each ticket is 10 dollars
-        (totalSeats in 1..60) -> 10
+    var price = 0
 
-        // Calculate - price 10 to front half of the rows and 8 dollars for the back half
-        (totalSeats > 60) -> if (ROW_NUMBER in firstHalf) 10 else 8
+    price = when {
+            // Calculate - price of each ticket is 10 dollars
+            (totalSeats in 1..60) -> 10
 
-        // Check any errors
-        else -> throw Exception("Error")
-    }
+            // Calculate - price 10 to front half of the rows and 8 dollars for the back half
+            (totalSeats > 60) -> if (ROW_NUMBER in firstHalf) 10 else 8
+
+            // Check any errors
+            else -> throw Exception("Error")
+        }
     SUM_PURCHASED_TICKETS += price
+    println("\nTicket price: $$price")
     return price
 }
 
-fun menu() {
-    println("\n1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit")
-    MENU_CHOICE = readln().toInt()
-    when (MENU_CHOICE) {
-        1 -> printCinema()
-        // Buy a ticket and save value
-        2 -> {
-            choseTicket()
-            println("Ticket price: $${priceTicket()}")
-            menu()
-        }
-        3 -> {
-            statistics()
-            menu()
-        }
-        // Exit from program and clear all
-        0 -> {
-            MENU_CHOICE = 0
-            ROWS = 0
-            SEATS = 0
-            ROW_NUMBER = 0
-            SEAT_NUMBER = 0
-            ALL_TICKETS.clear()
-        }
-    }
-}
-
 fun statistics() {
-    println("\nNumber of purchased tickets: ${ALL_TICKETS.size}")
-    println("Percentage: ${percentage()}%")
-    println("Current income: $SUM_PURCHASED_TICKETS")
-    println("Total income: ${income()}")
+    println("\nNumber of purchased tickets: ${ALL_TICKETS.size - 1}")
+    println("Percentage: ${"%.2f".format(percentage())}%")
+    println("Current income: $$SUM_PURCHASED_TICKETS")
+    println("Total income: $${income()}")
 }
 
-fun percentage(): Double {
-    TODO()
-}
+fun percentage(): Double = SUM_PURCHASED_TICKETS.toDouble() * 100.00 / income().toDouble()
 
 fun income() : Int {
     val totalSeats = ROWS * SEATS
-
 
     return when {
         // Calculate - price of each ticket is 10 dollars
